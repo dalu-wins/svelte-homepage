8f62bd78de417f97ebcbd25b4180e647e4b6dec8
Daniil Wins <dalu-wins@users.noreply.github.com>
2026-02-17 16:24:41 +0100

    fix: set up footer links to direct to github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -72,8 +72,8 @@
 <Footer class="m-5 mt-16 bg-gray-100">
 <FooterCopyright href="/" by="Daniil Wins" year={2026} />
 <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
-    <FooterLink href="/">Sourcecode</FooterLink>
-    <FooterLink href="/">Licensing</FooterLink>
+    <FooterLink href="https://github.com/dalu-wins/svelte-homepage">Sourcecode</FooterLink>
+    <FooterLink href="https://github.com/dalu-wins/svelte-homepage/blob/main/LICENSE">Licensing</FooterLink>
     <FooterLink href="#profile">Contact</FooterLink>
 </FooterLinkGroup>
 </Footer>
