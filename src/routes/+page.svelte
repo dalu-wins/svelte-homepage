8f822c05c046c0c615de7b44cca7a62e115ff467
Daniil Wins <dalu-wins@users.noreply.github.com>
2026-02-17 19:22:38 +0100

    fix: readd rounded footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -150,7 +150,7 @@ function checkCache(node: HTMLImageElement) {
     </Timeline>
 </div>
 
-<Footer class="mt-20 bg-gray-100 rounded-none">
+<Footer class="m-3 mt-20 bg-gray-100">
     <FooterCopyright href="/" by="Daniil Wins" year={2026} />
     <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
         <FooterLink href="https://github.com/dalu-wins/svelte-homepage">
