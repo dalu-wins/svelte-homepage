109b874c12ebf952680f770d111f7b5f0375fead
Dalu Wins <dalu-wins@users.noreply.github.com>
2026-02-25 21:19:37 +0100

    refactor: remove redundant div

--- src/lib/components/content/curriculum/Curriculum.svelte
+++ src/lib/components/content/curriculum/Curriculum.svelte
@@ -3,17 +3,15 @@
     import CurriculumEntry from "./CurriculumEntry.svelte";
 </script>
 
-<div class="rounded-4xl glass drop-shadow-md z-10">
-    <div class="rounded-4xl p-8 md:p-12 z-10">
-        <ul class="relative border-l-2 border-(--timeline-line) dark:border-(--timeline-line-dark) ml-24 sm:ml-32">
-            {#each curriculum as item}
-                <CurriculumEntry 
-                    title={item.title} 
-                    date={item.date}
-                    badge={item.type}
-                    description={item.description}
-                />
-            {/each}
-        </ul>
-    </div>
+<div class="rounded-4xl glass drop-shadow-md z-10 p-8 md:p-12">
+    <ul class="relative border-l-2 border-(--timeline-line) dark:border-(--timeline-line-dark) ml-24 sm:ml-32">
+        {#each curriculum as item}
+            <CurriculumEntry 
+                title={item.title} 
+                date={item.date}
+                badge={item.type}
+                description={item.description}
+            />
+        {/each}
+    </ul>
 </div>
